95a7449f791a99c38bc7da8d1ef9d955bd0b1702
shalinshah54-lab <shalinshah54@hotmail.com>
2021-05-26 19:18:46 +0000

    still tring to fix the google maps error for loading and the initMap function, unable to find and fix the problem.

--- assets/Script/map.js
+++ assets/Script/map.js
@@ -2,8 +2,8 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.7834345,  
+            lng: -73.9662495,
         }
     });
 }
