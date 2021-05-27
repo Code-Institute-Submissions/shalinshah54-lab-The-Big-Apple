1c4ef64d9d4d1d4d216511af5b75bf1214c0f894
shalinshah54-lab <shalinshah54@hotmail.com>
2021-05-27 15:44:32 +0000

    finished with the footer and added icons with social media links and added the copyrights, need to work on the google map with cluster marker. and putting the nieghborhood page as a popup page with extended information about the city.

--- assets/Script/map.js
+++ assets/Script/map.js
@@ -1,7 +1,7 @@
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 10,
         center: {
             lat: 40.7834345,  
             lng: -73.9662495,
