e8947409831408ee689d6ed694e721d6e449fd74
shalinshah54-lab <shalinshah54@hotmail.com>
2021-05-29 14:53:22 +0000

    added the cluster marker js, need to add the objects in the function for js.

--- assets/Script/map.js
+++ assets/Script/map.js
@@ -7,5 +7,21 @@ function initMap() {
             lng: -73.9662495,
         }
     });
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const markers = locations.map((location, i) => {
+        return new google.maps.Marker({
+          position: location,
+          label: labels[i % labels.length],
+        });
+      });
+      new MarkerClusterer(map, markers, {
+        imagePath:
+          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      });
 }
+const locations = [
+    { lat: -31.56391, lng: 147.154312 },
+
+  ];
+
   
