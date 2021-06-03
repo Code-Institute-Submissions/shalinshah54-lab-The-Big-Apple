5c07ed35c20da52d7a5e05581d3b1c624fc71933
shalinshah54-lab <shalinshah54@hotmail.com>
2021-06-03 18:55:57 +0000

    the contact page seems to be implementing properly based on the code and the styling and layout looks better.

--- assets/Script/email.js
+++ assets/Script/email.js
@@ -1,19 +1,20 @@
 function sendMail(contactForm) {
-    emailjs.send("service_hkgihaf","template_tlfrord", {
+    emailjs.send("gmail", "discoverwicklow", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("Message Sent", response);
-            document.getElementById("contactForm").reset();
-            document.getElementById("onSubmitText").innerText = "Your message has been sent!";
+        "from_message": contactForm.message.value
+      })
+      .then(
+        function (response) {
+          console.log("Message sent succesfully", response);
+          document.getElementById("contactForm").reset();
+          document.getElementById("onSubmitText").innerText = "Your message has been sent!";
         },
-        function(error) {
-            console.log("Message not Sent", error);
-            document.getElementById("onSubmitText").innerText = "Please ensure that all fields are filled in correctly";
-            document.getElementById("onSubmitText").style.color = "red";
-        });
+        function (error) {
+          console.log("Message not sent", error);
+          document.getElementById("onSubmitText").innerText = "Please ensure that all fields are filled in correctly";
+          document.getElementById("onSubmitText").style.color = "red";
+        }
+      );
     return false;
-}
+  }
